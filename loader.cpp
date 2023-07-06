7b7750f8fc56f851680d9af6bb277f7c58a7913f
d3adzo <enzo.destephano@gmail.com>
2023-07-06 18:09:38 -0400

    testing loader

---
+++ loader.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		LoadLibraryA("C:\\windows\\system32\\kgsgrd.dll"); // obfuscate
+		return FALSE;
+
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
